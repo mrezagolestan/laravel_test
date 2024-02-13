d27903bcc56998e724aec26182fb6c25c886a32b
Mohammad Reza Golestan <mreza_golestan@live.com>
2024-02-13 20:49:49 +0330

    add laravel breeze: authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 Route::redirect('/','products');
 
-Route::resource('products',\App\Http\Controllers\ProductController::class);
+Route::resource('products',
+    \App\Http\Controllers\ProductController::class)->middleware('auth');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
